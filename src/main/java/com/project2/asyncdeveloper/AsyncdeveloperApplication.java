00774a64fecaf8e905b07c50a2aad33f69270d56
Peter Farber <peterfarber@fullsail.edu>
2018-03-30 18:22:38 -0400

    Initial Commit (Uploading Base Project)

---
+++ src/main/java/com/project2/asyncdeveloper/AsyncdeveloperApplication.java
@@ -0,0 +1,12 @@
+package com.project2.asyncdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncdeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncdeveloperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project2/asyncdeveloper/AsyncdeveloperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project2.asyncdeveloper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsyncdeveloperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
